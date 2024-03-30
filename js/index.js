eea6ac3f9244e16d4ce6fac598203e030b3e6731
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-03-30 06:57:19 -0400

    using a redirect to load python server page

--- js/index.js
+++ js/index.js
@@ -1,14 +1,26 @@
-const portfolioButton =document.getElementById('portfolio-button');
-const serverLoadingMessage = document.getElementById('server-message');
-const containerDiv = document.querySelector(".container")
+// const portfolioButton =document.getElementById('portfolio-button');
+// const serverLoadingMessage = document.getElementById('server-message');
+// const containerDiv = document.querySelector(".container")
 
 
-portfolioButton.addEventListener("click", function(){
-  serverLoadingMessage.style.display = "block";
-  containerDiv.style.opacity = "0.3";
+// portfolioButton.addEventListener("click", function(){
+//   serverLoadingMessage.style.display = "block";
+//   containerDiv.style.opacity = "0.3";
 
+//   setTimeout(function() {
+//     serverLoadingMessage.style.display = "none";
+//     window.location.assign("https://heidis-python-portfolio.onrender.com/");
+//   }, 5000)
+// })
+
+function redirectToNewPage() {
   setTimeout(function() {
-    serverLoadingMessage.style.display = "none";
-    window.location.assign("https://heidis-python-portfolio.onrender.com/");
-  }, 5000)
-})
+    window.location.href = 'https://heidis-python-portfolio.onrender.com/';
+  }, 7000);
+}
+
+window.onload = function() {
+  if (window.location.pathname === '/loading-page.html') {
+    redirectToNewPage();
+  }
+};
