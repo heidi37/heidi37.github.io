fd68baccf0eea7265125ad3baa9324c414365fa9
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-05-03 21:56:48 -0400

    [BUGFIX] Removes unused javaScript

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-const flashingText = document.querySelector('.flashing-text');
-const texts = [
-  "Thank you for waiting",
-  "Free server is a little slow",
-  "🙏"
-];
-let index = 0;
-
-function changeText() {
-  flashingText.textContent = texts[index];
-  index = (index + 1) % texts.length;
-}
-
-setInterval(changeText, 5000);
-
 function redirectToNewPage() {
   setTimeout(function() {
     window.location.href = 'https://heidis-python-portfolio.onrender.com/';
